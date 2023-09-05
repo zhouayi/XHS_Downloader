6c6627984ab7d4bad8505a7ae182de17271b79be
zhouayi <54144475+zhouayi@users.noreply.github.com>
2023-09-05 10:37:34 +0800

    Update __init__.py

--- source/__init__.py
+++ source/__init__.py
@@ -7,6 +7,7 @@ from .Image import Image
 from .Manage import Manager
 from .Settings import Settings
 from .Video import Video
+import os
 
 
 class XHS:
@@ -28,17 +29,19 @@ class XHS:
         self.video = Video()
         self.explore = Explore()
         self.download = Download(path, folder, self.headers, proxies, chunk)
+        self.path = path
+        self.folder = folder
 
     def get_image(self, container: dict, html: str, download):
         urls = self.image.get_image_link(html)
         if download:
-            self.download.run(urls, container["作品ID"])
+            self.download.run(urls, container, container["作品类型"])
         container["下载地址"] = urls
 
     def get_video(self, container: dict, html: str, download):
         url = self.video.get_video_link(html)
         if download:
-            self.download.run(url, container["作品ID"])
+            self.download.run(url, container, container["作品类型"])
         container["下载地址"] = url
 
     def extract(self, url: str, download=False) -> dict:
@@ -48,10 +51,15 @@ class XHS:
         if not html:
             return {}
         data = self.explore.run(html)
+        path = os.path.join(self.path, self.folder, data["作品标题"] if len(data["作品标题"]) else data["作者昵称"])
+        if not os.path.exists(path):
+            os.mkdir(path)
         if data["作品类型"] == "视频":
             self.get_video(data, html, download)
         else:
             self.get_image(data, html, download)
+        print(f"用户：{data['作者昵称']}，笔记标题：{data['作品标题']} 下载完成")
+        print('--------------------------------------------------------------------------------')
         return data
 
     def check(self, url: str):

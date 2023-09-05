5504b2cf0b4afdf585c7329b2410c4c97f12bade
zhouayi <54144475+zhouayi@users.noreply.github.com>
2023-09-05 10:38:00 +0800

    Update Download.py

--- source/Download.py
+++ source/Download.py
@@ -3,6 +3,10 @@ from pathlib import Path
 from requests import exceptions
 from requests import get
 
+from PIL import Image
+from tqdm import tqdm
+import io
+
 
 class Download:
 
@@ -29,19 +33,35 @@ class Download:
             root.mkdir()
         return root
 
-    def run(self, urls: list, name: str):
-        if (l := len(urls)) > 1:
+    def run(self, urls: list, data: dict, type: str):
+        if type == "视频":
+            path = self.root.joinpath(data["作品标题"] if len(data["作品标题"]) else data["作者昵称"], data["作品ID"] + ".mp4")
+            self.download(urls[0], path, type)
+        else:
             for index, url in enumerate(urls):
-                self.download(url, f"{name}_{index + 1}.webp")
-        elif l == 1:
-            self.download(urls[0], f"{name}.mp4")
+                name = data["作品ID"]
+                path = self.root.joinpath(data["作品标题"] if len(data["作品标题"]) else data["作者昵称"], f"{name}_{index + 1}.jpg")
+                self.download(url, path, type)
+            
 
-    def download(self, url: str, name: str):
+    def download(self, url: str, path, type: str):
         try:
-            with get(url, headers=self.headers, proxies=self.proxies, stream=True) as response:
-                with self.root.joinpath(name).open("wb") as f:
-                    for chunk in response.iter_content(chunk_size=self.chunk):
-                        f.write(chunk)
-            print(f"{name} 下载成功！")
+            print(f"{type}开始下载", url)
+            if type == "视频":
+                with get(url, headers=self.headers, proxies=self.proxies, stream=True) as response:
+                    with path.open("wb") as f:
+                        for chunk in tqdm(response.iter_content(chunk_size=self.chunk)):
+                            f.write(chunk)
+            else:
+                response = get(url, headers=self.headers, proxies=self.proxies, stream=True)
+                img = Image.open(io.BytesIO(response.content))
+                img = img.convert('RGB')
+                img.save(path, "JPEG")
+                # with get(url, headers=self.headers, proxies=self.proxies, stream=True) as response:
+                #     for chunk in tqdm(response.iter_content(chunk_size=self.chunk)):
+                #         image = Image.open(io.BytesIO(chunk))
+                #         image.save(path, "JPEG")
+                
+            print(f"{type}下载完成！")
         except exceptions.ChunkedEncodingError:
-            print(f"网络异常，{name} 下载失败！")
+            print(f"网络异常，{path} 下载失败！")
